7530d8e841e34b152e05e3303cd2beadb70c12e9
sergey shishlov <shishlovsergey040593@gmail.com>
2025-02-28 16:40:12 +0200

    Ваше сообщение коммита

--- pages/ui/index.vue
+++ pages/ui/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>UI</div>
+  <div class="ui">UI</div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ui {
+  background-color: var(--color-ui-background);
+  height: 100vh;
+  padding: 124px 342px 57px 101px;
+}
+</style>
