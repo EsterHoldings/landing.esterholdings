7d2d35cb5c34ade3d7cb1f7ba17001baea74895b
rodionmaksimenko <asvrodya@gmail.com>
2025-06-29 14:49:46 +0300

    Updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,40 +61,27 @@ export default defineNuxtConfig({
         },
     },
 
-    runtimeConfig: {
-        recaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
-        public: {
-            baseApi: process.env.NUXT_PUBLIC_BASE_API,
-            baseUrl: process.env.NUXT_PUBLIC_BASE_URL,
-            cliFacebook: process.env.NUXT_PUBLIC_CLI_FACEBOOK,
-            cliGoogle: process.env.NUXT_PUBLIC_CLI_GOOGLE,
-            cliLinkIdIn: process.env.NUXT_PUBLIC_CLI_LINK_ID_IN,
-            reCaptchaSiteKey: process.env.NUXT_PUBLIC_RECAPTCHA_SITE_KEY,
-
-        }
-    },
-
     routeRules: {
         "/**": {ssr: false} as any,
         "/": {ssr: true} as any,
     },
 
-  app: {
-    head: {
-      script: [
-          {
-              src: "https://accounts.google.com/gsi/client",
-              async: true,
-              defer: true,
-          },
-          {
-              src: 'https://www.google.com/recaptcha/api.js?render=6LcxyW8rAAAAAB7veVQONzCAW9W1JBdWAXjHUg0P',
-              async: true,
-              defer: true,
-          },
-      ],
+    app: {
+        head: {
+            script: [
+                {
+                    src: "https://accounts.google.com/gsi/client",
+                    async: true,
+                    defer: true,
+                },
+                // {
+                //     src: 'https://www.google.com/recaptcha/api.js?render=6LcxyW8rAAAAAB7veVQONzCAW9W1JBdWAXjHUg0P',
+                //     async: true,
+                //     defer: true,
+                // },
+            ],
+        },
     },
-  },
 
     nitro: {
         devProxy: {
@@ -107,9 +94,16 @@ export default defineNuxtConfig({
     },
 
     runtimeConfig: {
+        recaptchaSecretKey: process.env.RECAPTCHA_SECRET_KEY,
         public: {
-            // apiUrl: process.env.NUXT_PUBLIC_API_URL,
             apiUrl: "https://esterholdings.website/api/",
+            baseApi: process.env.NUXT_PUBLIC_BASE_API,
+            baseUrl: process.env.NUXT_PUBLIC_BASE_URL,
+            cliFacebook: process.env.NUXT_PUBLIC_CLI_FACEBOOK,
+            cliGoogle: process.env.NUXT_PUBLIC_CLI_GOOGLE,
+            cliLinkIdIn: process.env.NUXT_PUBLIC_CLI_LINK_ID_IN,
+            reCaptchaSiteKey: process.env.NUXT_PUBLIC_RECAPTCHA_SITE_KEY,
+
         }
     }
 });
