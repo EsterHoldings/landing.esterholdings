fc48e6e44ebd9615270c70b798234f79bfe5d319
rodionmaksimenko <asvrodya@gmail.com>
2025-12-09 03:50:07 +0200

    Updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -225,6 +225,12 @@ export default defineNuxtConfig({
     app: {
         head: {
             link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico'}],
+            meta: [
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover',
+                },
+            ],
             script: [
                 {
                     src: 'https://accounts.google.com/gsi/client',
